071cbb29359d2a5b7ca59f65be2c253e5f588a31
oz <oz.rnds@gmail.com>
2024-01-04 23:02:48 +0200

    build(quick_build_deploy): Fix the IMAGE_NAME to fit the project naming convention

--- .autodevops/quick_build_deploy.sh
+++ .autodevops/quick_build_deploy.sh
@@ -8,7 +8,7 @@ source .autodevops/service.properties
 cz bump -ch
 
 SERVICE_VERSION=$(cz version -p)
-IMAGE_NAME=shkedia-photo-${COMPONENT_NAME}-service:${SERVICE_VERSION}
+IMAGE_NAME=${COMPONENT_NAME}-service:${SERVICE_VERSION}
 IMAGE_FULL_NAME=public.ecr.aws/q2n5r5e8/project-shkedia/${IMAGE_NAME}
 
 echo BUILD IMAGE $IMAGE_FULL_NAME
